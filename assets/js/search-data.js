266562f68a034915f6921d639ce0a203c794cd63
kamathhrishi <kamathhrishi@gmail.com>
2025-12-03 17:01:05 +0000

    Deploying to gh-pages from @ kamathhrishi/kamathhrishi.github.io@836a6ada4bf77b8c5303bc151cb79494195710ea 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,24 +3,24 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-",
+    title: "",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
+  },{id: "nav-",
+          title: "",
+          description: "A collection of my software engineering projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/blog/";
-          },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A collection of my software engineering projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
           },
         },];
