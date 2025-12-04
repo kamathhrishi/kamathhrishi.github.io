0cc244f78fc7c1a86e5d81dcf4a3c5d171149225
kamathhrishi <kamathhrishi@gmail.com>
2025-12-04 00:39:34 +0000

    Deploying to gh-pages from @ kamathhrishi/kamathhrishi.github.io@d593f7f18dc7c977bd24bee58a03f9738449858c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-projects",
           title: "projects",
-          description: "A collection of my software engineering projects.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
